b156ade9b96d668695011d8c79932d60644fa7a5
William Dutra <will.dutra26@gmail.com>
2024-08-13 18:15:38 -0300

    sql: inclusão da tabela categoria

--- sql/db.sql
+++ sql/db.sql
@@ -5,3 +5,10 @@ create table autor
 	email varchar(500) not null,
 	descricao varchar(400)
 );
+
+create table categoria
+(
+	id varchar(150) primary key,
+	nome varchar(150) not null,
+	descricao varchar(255)
+);
