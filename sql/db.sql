516115a6bea09b84fa8dec95a706ef2eb076e671
William Dutra <will.dutra26@gmail.com>
2024-08-19 15:03:03 -0300

    sql: inclusão da tabela de livros

--- sql/db.sql
+++ sql/db.sql
@@ -12,3 +12,16 @@ create table categoria
 	nome varchar(150) not null,
 	descricao varchar(255)
 );
+
+create table livro
+(
+	id varchar(150) primary key,
+	isbn varchar(150) not null,
+	titulo varchar(100) not null,
+	resumo varchar(500) not null,
+	sumario text,
+	numero_paginas int not null,
+	data_publicacao date,
+	categoria_id varchar(150) not null,
+	autor_id varchar(150) not null
+);
