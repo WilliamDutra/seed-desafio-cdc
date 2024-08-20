440c16dedfb953c9f2adfc25d356a3d3ad83e5ff
William Dutra <will.dutra26@gmail.com>
2024-08-20 18:05:13 -0300

    sql: inclusão das tabelas: pedido, item_pedido e cupom

--- sql/db.sql
+++ sql/db.sql
@@ -42,3 +42,39 @@ create table estado
 	sigla varchar(10) not null,
 	pais_id varchar(150) not null
 );
+
+create table pedido
+(
+	id varchar(150) primary key,
+	email varchar(100) not null,
+	nome varchar(100) not null,
+	sobrenome varchar(100) not null,
+	documento varchar(20) not null,
+	telefone varchar(15) not null,
+	logradouro varchar(255) not null,
+	cep varchar(10) not null,
+	cidade varchar(100) not null,
+	estado_id varchar(150) not null,
+	pais_id varchar(150) not null,
+	complemento varchar(100) null,
+	cupom_id varchar(150) not null,
+	total money not null
+);
+
+create table item_pedido 
+(
+	id varchar(150) primary key,
+	pedido_id varchar(150) not null,
+	quantidade int not null,
+	preco money not null,
+	total money not null
+);
+
+create table cupom 
+(
+	id varchar(150) primary key,
+	nome varchar(50) not null,
+	valor decimal not null,
+	validade date not null
+);
+
